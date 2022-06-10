3a2b5f63198be965120d62d2d282bdcb0cdffe31
Prateek Kumar Mishra <pr.prateekmishra@gmail.com>
2022-06-10 23:37:41 +0530

    fixed : section content hiding when navigating through sticky navbar

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ function toggle_bg(){
 
     nav.classList.toggle('scrolling-active', window.scrollY > 0);
 }
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
+// window.addEventListener("hashchange", function() { scrollBy(0, -50) });
